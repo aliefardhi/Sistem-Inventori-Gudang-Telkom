b2fc53c1743ecfd6b3cfe071ce36231eaab42928
aliefardhi <alief.118140003@student.itera.ac.id>
2021-06-15 17:05:18 +0700

    develop proses login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,9 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller 
 {
-    public function index()
+    public function __construct()
     {
-        $this->load->view('auth/login');
+        parent::__construct();
+        $this->load->library('form_validation');
     }
 
+    public function index()
+    {
+        $this->form_validation->set_rules('username','Username','required');
+        $this->form_validation->set_rules('password','Password','required');
+
+        if( $this->form_validation->run() == false){
+            $data['title'] = 'Selamat Datang!';
+            $this->load->view('templates/header', $data);
+            $this->load->view('login/login');
+            $this->load->view('templates/footer');
+        }
+    }
 }
--- application/views/login/login.php
+++ application/views/login/login.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Login Page</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
-
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    
-    <!-- My Style -->
-    <link rel="stylesheet" href="<?= base_url('assets/mystyle.css') ?>">
-
-</head>
-
 <body class="">
     
     <h1 class="judul text-center h4 text-gray-900 mb-4 mt-5">SISTEM INVENTORI BARANG GUDANG</h1>
@@ -48,24 +18,32 @@
                                     <div class="text-center mb-2">
                                         <img src="./assets/logo.png" alt="logo" width="215" height="208">
                                     </div>
-                                    <form class="user">
+
+                                    <?= $this->session->flashdata('message'); ?>
+
+                                    <form class="user" method="POST" action="<?= base_url(); ?>auth">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user"
                                                 id="username"
                                                 name="username"
                                                 placeholder="Username">
+                                                <?= form_error('username','<small class="text-danger pl-3">','</small>') ?>
                                         </div>
+
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
                                                 id="password" 
                                                 name="password"
                                                 placeholder="Password">
+                                                <?= form_error('password','<small class="text-danger pl-3">','</small>') ?>
                                         </div>
+
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                             </div>
                                         </div>
+
                                         <button type="submit" class="btn btnlogin btn-user btn-block ">
                                             Login
                                         </button>
@@ -83,16 +61,4 @@
         </footer>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
+    
