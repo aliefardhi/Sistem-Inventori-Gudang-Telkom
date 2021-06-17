2773437d684f5b4d6063bceb1b2621a14bf6c1ff
aliefardhi <alief.118140003@student.itera.ac.id>
2021-06-17 14:11:31 +0700

    added datatables barang keluar

--- application/controllers/Barang_keluar.php
+++ application/controllers/Barang_keluar.php
@@ -3,12 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Barang_keluar extends CI_Controller 
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('inv_model');
+    }
     public function index(){
-        // $data['user'] = $this->db->get_where('user',['username' => $this->session->userdata('username')])->row_array();
-        // echo "Selamat datang " . $data['user']['username'];
+        $data['b_keluar'] = $this->inv_model->tampil_data_keluar();
         $data['title'] = 'Daftar barang keluar';
         $this->load->view('templates/header', $data);
-        $this->load->view('barang_keluar/barang_keluar');
+        $this->load->view('barang_keluar/barang_keluar', $data);
         $this->load->view('templates/footer');
     }
 
--- application/views/barang_keluar/barang_keluar.php
+++ application/views/barang_keluar/barang_keluar.php
@@ -4,11 +4,11 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class=" sidebar navbar-nav  sidebar sidebar-dark accordion mb-3" id="accordionSidebar">
+        <ul class=" sidebar navbar-nav  sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Nav Item Logo--->
             <li class="logosidebar">
-            <img src="./assets/logo.png" alt="logo" width="125" height="125">
+                <img src="./assets/logo.png" alt="logo" width="125" height="125">
             </li> 
         
             <!-- Divider -->
@@ -40,10 +40,10 @@
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex mt-3 flex-column">
+        <div id="content-wrapper" class="d-flex flex-column">
 
             <!-- Page Heading -->
-            <h1 class="judul h3 mb-5 text-center">DAFTAR BARANG KELUAR</h1>
+            <h1 class="judul h3 mb-5 text-center mt-4">DAFTAR BARANG MASUK</h1>
 
             <!-- button tambah dan explore-->
             <div>
@@ -56,8 +56,44 @@
                 </button>
             </div>
 
+            <div class="col">
+                <!-- Datatables -->
+                <table id="mytable" class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>No.</th>
+                            <th>WH Asal</th>
+                            <th>SN</th>
+                            <th>MAC</th>
+                            <th>Tanggal Kirim</th>
+                            <th>WH Tujuan</th>
+                            <th>Jumlah Keluar</th>
+                            <th>Jenis</th>
+                            <th>Tipe</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($b_keluar as $bk){ ?>
+                        <tr>
+                            <td><?= $bk->id_keluar; ?></td>
+                            <td><?= $bk->wh_asal; ?></td>
+                            <td><?= $bk->sn_keluar; ?></td>
+                            <td><?= $bk->mac_keluar; ?></td>
+                            <td><?= $bk->tgl_kirim; ?></td>
+                            <td><?= $bk->wh_tujuan; ?></td>
+                            <td><?= $bk->jumlah_keluar; ?></td>
+                            <td><?= $bk->jenis_keluar; ?></td>
+                            <td><?= $bk->tipe_keluar; ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+                <!-- End of Datatables -->
+            </div>
         </div>
         <!-- End of Content Wrapper -->
+
+
 
     </div>
     <!-- End of Page Wrapper -->
