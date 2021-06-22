dfbc057977acae62fce922ba3bba01bc397a286e
AnnisaJufeAryani <65056172+AnnisaJufeAryani@users.noreply.github.com>
2021-06-22 08:33:58 +0700

    add controller edit dan develop edit barang keluar

---
+++ application/controllers/Edit_keluar.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Edit_keluar extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('inv_model');
+    }
+
+    public function index(){
+        $data['b_masuk'] = $this->inv_model->tampil_data();
+        $data['title'] = 'Edit barang keluar';
+        $this->load->view('templates/header', $data);
+        $this->load->view('barang_keluar/edit_keluar', $data);
+        $this->load->view('templates/footer');
+    }
+
+}
